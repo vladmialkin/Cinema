b37a2b70a243f017ecaf86979714b8c40a45d90d
programmer <myalkin.vlad@yandex.ru>
2022-12-25 17:00:00 +0300

    - Изменение получения списка залов для выпадающего списка и мест таблицы по id сеанса; - Добалвение функции получения фильмов для выпадающего списка; - Добавление функции получения сессии для таблицы.

--- models/model_customer.py
+++ models/model_customer.py
@@ -12,9 +12,9 @@ class ModelCustomer:
             names.append(value[1])
         return names, id_genre
 
-    def get_halls_to_dropdown(self):
+    def get_halls_to_dropdown(self, session_id: int):
         """функция получает данные всех залов"""
-        halls = self.database.get_halls()
+        halls = self.database.get_halls(session_id=session_id)
         id_halls = []
         names = []
         for value in halls:
@@ -22,9 +22,9 @@ class ModelCustomer:
             names.append(value[1])
         return names, id_halls
 
-    def get_places_to_hall_table(self, id_hall):
+    def get_places_to_hall_table(self, id_hall, session_id):
         """функция получает данные мест зала"""
-        places = self.database.get_places(id_hall=id_hall)
+        places = self.database.get_places(id_hall=id_hall, session_id=session_id)
         data = []
 
         hall = self.database.get_hall(id_hall)[0]
@@ -61,3 +61,20 @@ class ModelCustomer:
             row_data.update(dict)
             data.append(row_data)
         return data
+
+    def get_films_to_dropdown(self, session_id: int, hall_id: int):
+        """функция получает данные всех фильмов зала"""
+        films = self.database.get_films(session_id=session_id, hall_id=hall_id)
+        id_films = []
+        names = []
+        for value in films:
+            id_films.append(value[0])
+            names.append(value[1])
+        return names, id_films
+
+    def get_session_to_hall_table(self, session_id):
+        """функция получает данные сеанса по id"""
+        session = self.database.get_session(session_id=session_id)
+        session_time = session[1]
+        session_start = session_time.split("-")[0]
+        return session_start, session_time
